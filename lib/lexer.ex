e6e4bcae29075c4523a80b45e27b2535265d39f6
Jake Norton <jake.norton88@gmail.com>
2025-02-16 16:28:32 +1300

    refactor(cleanup): change to maps for lexer

--- lib/lexer.ex
+++ lib/lexer.ex
@@ -2,176 +2,49 @@ defmodule Shell.Lexer do
   alias Shell.Token
   alias Shell.Position
 
+  @symbols %{
+    ?. => :dot,
+    ?= => :equals,
+    ?+ => :plus,
+    ?- => :plus,
+    ?* => :asterix,
+    ?/ => :fslash,
+    ?\\ => :bslash,
+    ?( => :lparen,
+    ?) => :rparen,
+    ?{ => :lbrace,
+    ?} => :rbrace,
+    ?' => :squote,
+    ?" => :dquote,
+    ?? => :question_mark,
+    ?! => :bang,
+    ?\n => :newline
+  }
+
+  # Validate that all symbol types are valid token types
+  for {_char, type} <- @symbols do
+    unless Token.valid_type?(type) do
+      raise "Invalid token type #{type} in symbols map"
+    end
+  end
+
+  @keywords %{
+    "let" => :let,
+    "fn" => :fn,
+    "return" => :return
+  }
+
+  # Validate that all keyword types are valid token types
+  for {_word, type} <- @keywords do
+    unless Token.valid_type?(type) do
+      raise "Invalid token type #{type} in keywords map"
+    end
+  end
+
   defstruct rest: <<>>,
             position: %Position{},
             curr_token: {<<>>, :none, %Position{}},
             tokens: [%Token{type: :ident}]
-
-  def tokenize(%__MODULE__{
-        rest: <<>>,
-        position: end_pos,
-        curr_token: {<<>>, :none, _position},
-        tokens: acc
-      }),
-      do: {Enum.reverse(acc), end_pos}
-
-  def tokenize(%__MODULE__{
-        rest: <<>>,
-        position: end_pos,
-        curr_token: {curr, type, position},
-        tokens: acc
-      }),
-      do: {Enum.reverse(make_and_add_token({curr, type, position}, acc)), end_pos}
-
-  def tokenize(%__MODULE__{
-        rest: <<?\s, rest::bitstring>>,
-        position: lexer_pos,
-        curr_token: {curr, type, position},
-        tokens: acc
-      }) do
-    new_pos = increment_col(lexer_pos)
-
-    tokenize(%__MODULE__{
-      rest: rest,
-      position: new_pos,
-      curr_token: {<<>>, :none, new_pos},
-      tokens: make_and_add_token({curr, type, position}, acc)
-    })
-  end
-
-  def tokenize(%__MODULE__{
-        rest: <<char::utf8, rest::bitstring>>,
-        position: lexer_pos,
-        curr_token: {curr, type, position},
-        tokens: acc
-      })
-      when char in ?a..?z or
-             char in ?A..?Z do
-    new_pos = increment_col(lexer_pos)
-
-    case type do
-      :none ->
-        tokenize(%__MODULE__{
-          rest: rest,
-          position: new_pos,
-          curr_token: {<<curr::binary, char::utf8>>, :alpha, lexer_pos},
-          tokens: acc
-        })
-
-      :alpha ->
-        tokenize(%__MODULE__{
-          rest: rest,
-          position: new_pos,
-          curr_token: {<<curr::binary, char::utf8>>, :alpha, position},
-          tokens: acc
-        })
-
-      :number ->
-        tokenize(%__MODULE__{
-          rest: rest,
-          position: new_pos,
-          curr_token: {<<curr::binary, char::utf8>>, :alphanumeric, position},
-          tokens: acc
-        })
-
-      :alphanumeric ->
-        tokenize(%__MODULE__{
-          rest: rest,
-          position: new_pos,
-          curr_token: {<<curr::binary, char::utf8>>, :alphanumeric, position},
-          tokens: acc
-        })
-    end
-  end
-
-  def tokenize(%__MODULE__{
-        rest: <<char::utf8, rest::bitstring>>,
-        position: lexer_pos,
-        curr_token: {curr, type, position},
-        tokens: acc
-      })
-      when char in ?0..?9 do
-    new_pos = increment_col(lexer_pos)
-
-    case type do
-      :none ->
-        tokenize(%__MODULE__{
-          rest: rest,
-          position: new_pos,
-          curr_token: {<<curr::binary, char::utf8>>, :number, lexer_pos},
-          tokens: acc
-        })
-
-      :alpha ->
-        tokenize(%__MODULE__{
-          rest: rest,
-          position: new_pos,
-          curr_token: {<<curr::binary, char::utf8>>, :number, position},
-          tokens: acc
-        })
-
-      :number ->
-        tokenize(%__MODULE__{
-          rest: rest,
-          position: new_pos,
-          curr_token: {<<curr::binary, char::utf8>>, :alphanumeric, position},
-          tokens: acc
-        })
-
-      :alphanumeric ->
-        tokenize(%__MODULE__{
-          rest: rest,
-          position: new_pos,
-          curr_token: {<<curr::binary, char::utf8>>, :alphanumeric, position},
-          tokens: acc
-        })
-    end
-  end
-
-  def tokenize(%__MODULE__{
-        rest: <<char::utf8, rest::bitstring>>,
-        position: lexer_pos,
-        curr_token: {curr, type, position},
-        tokens: acc
-      }) do
-    _prev =
-      case type do
-        :none ->
-          :ok
-
-        _ ->
-          make_and_add_token({curr, type, position}, acc)
-      end
-
-    token =
-      case char do
-        ?. -> {to_char(char), :dot}
-        ?= -> {to_char(char), :equals}
-        ?* -> {to_char(char), :asterix}
-        ?/ -> {to_char(char), :fslash}
-        ?\\ -> {to_char(char), :bslash}
-        ?( -> {to_char(char), :lparen}
-        ?) -> {to_char(char), :rparen}
-        ?{ -> {to_char(char), :lbrace}
-        ?} -> {to_char(char), :rbrace}
-        ?' -> {to_char(char), :squote}
-        ?" -> {to_char(char), :dquote}
-        ?? -> {to_char(char), :question_mark}
-        ?! -> {to_char(char), :bang}
-        ?\n -> {to_char(char), :newline}
-        _ -> {to_char(char), :unlexible}
-      end
-
-    {curr, type} = token
-    new_pos = increment_col(lexer_pos)
-
-    tokenize(%__MODULE__{
-      rest: rest,
-      position: new_pos,
-      curr_token: {<<>>, :none, new_pos},
-      tokens: make_and_add_token({curr, type, position}, acc)
-    })
-  end
 
   def lex(input) do
     tokenize(%__MODULE__{
@@ -182,26 +55,136 @@ defmodule Shell.Lexer do
     })
   end
 
-  defp make_and_add_token({curr, type, position}, acc) do
-    case type do
-      :none ->
-        acc
+  # End of the tokens 
+  def tokenize(%__MODULE__{
+        rest: <<>>,
+        position: end_pos,
+        curr_token: {<<>>, :none, _position},
+        tokens: acc
+      }),
+      do: {Enum.reverse(acc), end_pos}
 
-      :alpha ->
-        case curr do
-          "let" -> [Token.new(curr, :let, position) | acc]
-          "fn" -> [Token.new(curr, :fn, position) | acc]
-          _ -> [Token.new(curr, type, position) | acc]
-        end
+  # Last token
+  def tokenize(%__MODULE__{
+        rest: <<>>,
+        position: end_pos,
+        curr_token: {curr, type, position},
+        tokens: acc
+      }),
+      do: {Enum.reverse(make_and_add_token({curr, type, position}, acc)), end_pos}
 
-      _ ->
-        [Token.new(curr, type, position) | acc]
+  # End current token with space
+  # TODO: Handle other whitespace
+  def tokenize(%__MODULE__{
+        rest: <<char, rest::bitstring>>,
+        position: lexer_pos,
+        curr_token: {curr, type, position},
+        tokens: acc
+      })
+      when char in [?\s, ?\t] do
+    new_pos = increment_col(lexer_pos)
+
+    tokenize(%__MODULE__{
+      rest: rest,
+      position: new_pos,
+      curr_token: {<<>>, :none, new_pos},
+      tokens: make_and_add_token({curr, type, position}, acc)
+    })
+  end
+
+  # Combined handling for alphanumeric characters
+  def tokenize(%__MODULE__{
+        rest: <<char::utf8, rest::bitstring>>,
+        position: lexer_pos,
+        curr_token: {curr, type, position},
+        tokens: acc
+      })
+      when char in ?a..?z or char in ?A..?Z or char in ?0..?9 do
+    new_pos = increment_col(lexer_pos)
+    new_type = determine_type(char, type)
+    token_position = get_position(type, lexer_pos, position)
+
+    tokenize(%__MODULE__{
+      rest: rest,
+      position: new_pos,
+      curr_token: {<<curr::binary, char::utf8>>, new_type, token_position},
+      tokens: acc
+    })
+  end
+
+  # Symbol handling
+  def tokenize(%__MODULE__{
+        rest: <<char::utf8, rest::bitstring>>,
+        position: lexer_pos,
+        curr_token: {curr, type, position},
+        tokens: acc
+      }) do
+    tokens =
+      case type do
+        :none -> acc
+        _ -> make_and_add_token({curr, type, position}, acc)
+      end
+
+    {token_value, token_type} =
+      case Map.get(@symbols, char) do
+        nil -> {to_char(char), :unlexible}
+        symbol_type -> {to_char(char), symbol_type}
+      end
+
+    new_pos = increment_col(lexer_pos)
+
+    tokenize(%__MODULE__{
+      rest: rest,
+      position: new_pos,
+      curr_token: {<<>>, :none, new_pos},
+      tokens: make_and_add_token({token_value, token_type, lexer_pos}, tokens)
+    })
+  end
+
+  # Helper functions
+  defp determine_type(char, current_type) when char in ?0..?9 do
+    case current_type do
+      :none -> :number
+      :alpha -> :alphanumeric
+      type -> type
     end
   end
+
+  defp determine_type(_char, current_type) do
+    case current_type do
+      :none -> :alpha
+      type -> type
+    end
+  end
+
+  defp get_position(:none, lexer_pos, _position), do: lexer_pos
+  defp get_position(_type, _lexer_pos, position), do: position
+
+  defp make_and_add_token({curr, :alpha, position}, acc) do
+    type = Map.get(@keywords, curr, :ident)
+    [Token.new(curr, type, position) | acc]
+  end
+
+  defp make_and_add_token({curr, :alphanumeric, position}, acc) do
+    type = Map.get(@keywords, curr, :ident)
+    [Token.new(curr, type, position) | acc]
+  end
+
+  defp make_and_add_token({curr, type, position}, acc) when type != :none do
+    [Token.new(curr, type, position) | acc]
+  end
+
+  defp make_and_add_token({_curr, :none, _position}, acc), do: acc
 
   defp to_char(char), do: <<char::utf8>>
 
   defp increment_col(%Position{file: file, row: row, col: col}) do
     %Position{file: file, row: row, col: col + 1}
+  end
+
+  # And when you add line breaks
+  defp increment_row(%Position{file: file, row: row}) do
+    # Reset column to 1 on new line
+    %Position{file: file, row: row + 1, col: 1}
   end
 end
