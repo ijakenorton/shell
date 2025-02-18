92ae82f6e2a85d1b40abbd5283df15519f147532
Jake Norton <jake.norton88@gmail.com>
2025-02-18 16:54:01 +1300

    update idents to have clear functionality

--- lib/idents.ex
+++ lib/idents.ex
@@ -9,10 +9,15 @@ defmodule Shell.Idents do
     Agent.get(__MODULE__, &Map.get(&1, key))
   end
 
-  def put_ident(key, token) do
-    Agent.update(__MODULE__, &Map.put(&1, key, token))
+  def put_ident(key, value) do
+    Agent.update(__MODULE__, &Map.put(&1, key, value))
   end
 
-  def inspect_ident() do
+  def inspect_idents do
+    Agent.get(__MODULE__, & &1)
+  end
+
+  def clear do
+    Agent.update(__MODULE__, fn _state -> %{} end)
   end
 end
