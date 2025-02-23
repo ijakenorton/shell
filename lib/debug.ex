72dae29936a7faa75123878194acd7d5ca116103
Jake Norton <jake.norton88@gmail.com>
2025-02-23 20:29:59 +1300

    changed debug to inspect as that is the current use case

--- lib/debug.ex
+++ lib/debug.ex
@@ -1,7 +1,11 @@
 defmodule Shell.Debug do
+  require Logger
+
   defmacro debug_log(message) do
     quote do
-      Logger.warning("#{__MODULE__} at #{__ENV__.file}:#{__ENV__.line}: #{unquote(message)}")
+      Logger.warning(
+        "#{__MODULE__} at #{__ENV__.file}:#{__ENV__.line}: #{inspect(unquote(message), pretty: true)}"
+      )
     end
   end
 end
